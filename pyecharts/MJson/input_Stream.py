8aa2c4dde451096e3a7629e7721dbd3f5d1c0818
ZoMaii <xiaohunzomaii@gmail.com>
2024-10-20 20:17:32 +0800

    兼容配置

--- pyecharts/MJson/input_Stream.py
+++ pyecharts/MJson/input_Stream.py
@@ -1,7 +1,14 @@
 import json
+import re
 
 def fileJsonOpen(URL,Way='r',code='utf-8'):
-    with open(URL,Way,encoding=code) as file:
-        str = file.readlines()[0].replace('jsonp_1629350871167_29498(',"")[:-2]
-        return json.loads(str)
+    with open(URL, Way, encoding=code) as file:
+        str = file.readlines()[0]
+        str = re.sub(r'^[^(]+\((.*)\);?$', r'\1', str)
 
+        # print("JSON text:", str)
+        try:
+            return json.loads(str)
+        except json.decoder.JSONDecodeError as e:
+            print(f"JSON 解码错误: {e}")
+            return None
